7c3038949e0a8c156bbf025a58dffec5af91321a
emperoroficecream <ebisu1922@gmail.com>
2017-08-06 16:39:33 -0400

    added multiplication assembly code

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,28 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Add R0 R1 times.
+	@2 // R2 = 0
+	M=0
+	@i // Set counter to 0
+	M=0
+
+(LOOP)
+	@i
+	D=M
+	@1
+	D=D-M // D = i - R1
+	@END
+	D;JGE
+
+	@0
+	D=M // D = R0
+	@2
+	M=D+M // R2 = R2 + R0
+	@i
+	M=M+1 // Increment counter
+	@LOOP
+	0;JMP
+(END)
+	@END
+	0;JMP
